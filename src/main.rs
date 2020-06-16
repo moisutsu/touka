34f7f3f3415f1bc0ed94e06ef722131953722a72
moisutsu <moisutsu@gmail.com>
2020-06-17 02:15:58 +0900

    [Add] 白かを判定するためのしきい値をコマンドライン引数で指定する機能を追加

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,22 @@ fn main() {
                 .help("Output image path.")
                 .short("o")
                 .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("threshold")
+                .help("A threshold to determine if a color is white.")
+                .short("t")
+                .takes_value(true),
         );
 
     let matches = app.get_matches();
     let input_path = matches.value_of("input_path").unwrap();
     let output_path = matches.value_of("output_path").unwrap_or("output");
+    let threshold: u8 = matches
+        .value_of("threshold")
+        .unwrap_or("230")
+        .parse()
+        .unwrap();
 
     let mut img = image::open(&input_path).unwrap().to_rgba();
     let (width, height) = img.dimensions();
@@ -29,7 +40,7 @@ fn main() {
         for y in 0..height {
             let pixel = img.get_pixel(x, y);
             let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
-            if is_white(r, g, b) {
+            if is_white(r, g, b, threshold) {
                 img.put_pixel(x, y, Rgba([r, g, b, 0]));
             }
         }
@@ -38,7 +49,6 @@ fn main() {
 }
 
 #[allow(unused_comparisons)]
-fn is_white(r: u8, g: u8, b: u8) -> bool {
-    let threshold = 230;
+fn is_white(r: u8, g: u8, b: u8, threshold: u8) -> bool {
     threshold <= r && r <= 255 && threshold <= g && g <= 255 && threshold <= b && b <= 255
 }
