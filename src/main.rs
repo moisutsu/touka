e1229285d93e77b5a0496e47d010c5539bb8d4a7
moisutsu <moisutsu@gmail.com>
2020-06-17 01:34:12 +0900

    [Add] 画像の背景を透過する機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 extern crate image;
 
+use image::Rgba;
+
 fn main() {
-    let img = image::open("images/dog.png").unwrap().to_rgb();
+    let mut img = image::open("images/dog.png").unwrap().to_rgba();
     let (width, height) = img.dimensions();
     for x in 0..width {
         for y in 0..height {
             let pixel = img.get_pixel(x, y);
-            println!("{:?}", pixel);
+            let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
+            if is_white(r, g, b) {
+                img.put_pixel(x, y, Rgba([r, g, b, 0]));
+            }
         }
     }
+    img.save("images/output.png").unwrap();
+}
+
+#[allow(unused_comparisons)]
+fn is_white(r: u8, g: u8, b: u8) -> bool {
+    let threshold = 230;
+    threshold <= r && r <= 255 && threshold <= g && g <= 255 && threshold <= b && b <= 255
 }
