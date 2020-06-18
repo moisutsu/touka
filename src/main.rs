f48d2f0ef520b79d02e3a8544d26a8b13926743e
moisutsu <moisutsu@gmail.com>
2020-06-18 22:17:54 +0900

    [Improve] #2 std::iter::Zipによる二重ループの解消

--- src/main.rs
+++ src/main.rs
@@ -41,13 +41,11 @@ fn main() {
 
     let mut img = image::open(&input_path).unwrap().to_rgba();
     let (width, height) = img.dimensions();
-    for x in 0..width {
-        for y in 0..height {
-            let pixel = img.get_pixel(x, y);
-            let &Rgba([r, g, b, _]) = pixel;
-            if is_white(r, g, b, threshold) {
-                img.put_pixel(x, y, Rgba([r, g, b, 0]));
-            }
+    for (x, y) in (0..width).zip(0..height) {
+        let pixel = img.get_pixel(x, y);
+        let &Rgba([r, g, b, _]) = pixel;
+        if is_white(r, g, b, threshold) {
+            img.put_pixel(x, y, Rgba([r, g, b, 0]));
         }
     }
     img.save(format!("{}.png", output_path)).unwrap();
